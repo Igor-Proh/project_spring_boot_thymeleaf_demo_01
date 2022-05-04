a6a0e8cf440a1250d88d6bff0d5c4ac4c353725d
Prokhnov Igor <71402291+Igor-Proh@users.noreply.github.com>
2022-05-04 13:29:22 +0300

    Add files via upload

---
+++ thymeleaf_demo_01_08_employees_delete/src/main/java/com/prokhnov/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,51 @@
+package com.prokhnov.springboot.thymeleafdemo.controller;
+
+import com.prokhnov.springboot.thymeleafdemo.entity.Employee;
+import com.prokhnov.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        model.addAttribute("employees", employeeService.findAll());
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+        Employee employee = employeeService.findById(id);
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int id){
+        employeeService.deleteById(id);
+        return "redirect:/employees/list";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employees/list";
+    }
+}
---
+++ thymeleaf_demo_01_08_employees_delete/src/main/java/com/prokhnov/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.prokhnov.springboot.thymeleafdemo.dao;
+
+import com.prokhnov.springboot.thymeleafdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByOrderByLastNameAsc();
+	
+}
